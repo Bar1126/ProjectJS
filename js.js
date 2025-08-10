84e189f8e9a6846cdb9467b75a140587ba559a84
Bar1126 <barsho26@gmail.com>
2025-08-10 10:35:14 +0300

    project.path#hagana
    hagana om project

--- js.js
+++ js.js
@@ -1,16 +1,16 @@
-'use strict'
+'use strict';
 
 //add contact popup
-const addContactBtn = document.getElementById("addContactBtnPopup");
-const addContactPopup = document.getElementById("addContact");
-addContactBtn.addEventListener('click', (e) => {
-  e.preventDefault()
-  addContactPopup.style.display = 'block';
-})
-const closeAddPopUpBtn = document.getElementById("closeAddPopUpBtn");
-closeAddPopUpBtn.addEventListener('click', (e) => {
+const addContactBtn = document.getElementById('addContactBtnPopup');
+const addContactPopup = document.getElementById('addContact');
+addContactBtn.addEventListener('click', e => {
   e.preventDefault();
-  addContactPopup.style.display = 'none'
+  addContactPopup.style.display = 'block';
+});
+const closeAddPopUpBtn = document.getElementById('closeAddPopUpBtn');
+closeAddPopUpBtn.addEventListener('click', e => {
+  e.preventDefault();
+  addContactPopup.style.display = 'none';
 });
 
 //contact list
@@ -44,7 +44,7 @@ let contactList = [
     phoneNum: '+972544971252',
     age: 23,
     email: 'sean.lew@gmail.com',
-    freeText: "hello :)",
+    freeText: 'hello :)',
     dateAdded: new Date(),
   },
 ];
@@ -58,13 +58,11 @@ const sortContacts = () => {
   //empty list message
   const emptyMessage = document.getElementById('emptyMessage');
   if (contactList.length === 0) {
-    emptyMessage.style.display = 'block'
+    emptyMessage.style.display = 'block';
     return;
+  } else {
+    emptyMessage.style.display = 'none';
   }
-  else {
-    emptyMessage.style.display = 'none'
-  }
-
 
   contactList.forEach((contact, index) => {
     const li = document.createElement('li');
@@ -89,8 +87,7 @@ const sortContacts = () => {
 sortContacts();
 
 ul.addEventListener('click', e => {
-  if (e.target.tagName !== 'BUTTON')
-    return;
+  if (e.target.tagName !== 'BUTTON') return;
   const action = e.target.textContent.trim().toLowerCase();
   const li = e.target.closest('li');
   const index = parseInt(li.getAttribute('data-id'));
@@ -98,11 +95,21 @@ ul.addEventListener('click', e => {
 
   if (action === 'info') {
     document.getElementById('name').textContent = contact.name;
-    document.getElementById('contactInfoP1').textContent = `Phone: ${contact.phoneNum}`;
-    document.getElementById('contactInfoP2').textContent = `Age: ${contact.age}`;
-    document.getElementById('contactInfoP3').textContent = `Email: ${contact.email}`;
-    document.getElementById('contactInfoP4').textContent = `Note: ${contact.freeText}`;
-    document.getElementById('contactInfoP5').textContent = `Added: ${contact.dateAdded.toLocaleString()}`;
+    document.getElementById(
+      'contactInfoP1',
+    ).textContent = `Phone: ${contact.phoneNum}`;
+    document.getElementById(
+      'contactInfoP2',
+    ).textContent = `Age: ${contact.age}`;
+    document.getElementById(
+      'contactInfoP3',
+    ).textContent = `Email: ${contact.email}`;
+    document.getElementById(
+      'contactInfoP4',
+    ).textContent = `Note: ${contact.freeText}`;
+    document.getElementById(
+      'contactInfoP5',
+    ).textContent = `Added: ${contact.dateAdded.toLocaleString()}`;
     document.getElementById('contactInfo').style.display = 'block';
   }
 
@@ -112,7 +119,9 @@ ul.addEventListener('click', e => {
     document.getElementById('editNum').value = contact.phoneNum;
     document.getElementById('editEmail').value = contact.email;
     document.getElementById('editFreeText').value = contact.freeText;
-    document.getElementById('contactInfoP5').textContent = `Edited: ${contact.dateAdded = new Date()}`
+    document.getElementById(
+      'contactInfoP5',
+    ).textContent = `Edited: ${(contact.dateAdded = new Date())}`;
     document.getElementById('contactId').setAttribute('data-id', index);
     document.getElementById('editContact').style.display = 'block';
   }
@@ -124,13 +133,59 @@ ul.addEventListener('click', e => {
 
 document.getElementById('editContactBtn').onclick = e => {
   e.preventDefault();
-  const index = parseInt(document.getElementById('contactId').getAttribute('data-id'));
+  const index = parseInt(
+    document.getElementById('contactId').getAttribute('data-id'),
+  );
 
   contactList[index].name = document.getElementById('editName').value.trim();
+  if (contactList[index].name === '') {
+    confirm('name cannot be void');
+    while (confirmed) {
+      contactList[index].name = document
+        .getElementById('editName')
+        .value.trim();
+    }
+  }
   contactList[index].age = parseInt(document.getElementById('editAge').value);
+  if (
+    contactList[index].age < 0 ||
+    document.getElementById('editAge').value > 120
+  ) {
+    confirm('invalid age');
+    while (confirmed) {
+      contactList[index].age = parseInt(
+        document.getElementById('editAge').value,
+      );
+    }
+  }
   contactList[index].phoneNum = document.getElementById('editNum').value.trim();
+
+  if (contactList[index].phoneNum.length != 10) {
+    confirm('phone number must contain 10 digits');
+    while (confirmed) {
+      contactList[index].phoneNum = document
+        .getElementById('editNum')
+        .value.trim();
+    }
+  }
+  for (let i = 0; i < contactList[index].phoneNum.length; i++) {
+    if (
+      contactList[index].phoneNum[i] < '0' ||
+      contactList[index].phoneNum[i] > '9'
+    ) {
+      confirm('phone number must only contain numbers!');
+      while (confirmed) {
+        contactList[index].phoneNum = document
+          .getElementById('editNum')
+          .value.trim();
+      }
+    }
+  }
+
   contactList[index].email = document.getElementById('editEmail').value.trim();
-  contactList[index].freeText = document.getElementById('editFreeText').value.trim();
+  contactList[index].freeText = document
+    .getElementById('editFreeText')
+    .value.trim();
 
   document.getElementById('editContact').style.display = 'none';
   sortContacts();
@@ -138,18 +193,18 @@ document.getElementById('editContactBtn').onclick = e => {
 
 document.querySelector('.addContactForm').onsubmit = e => {
   e.preventDefault();
-  const name = document.getElementById("addName").value.trim();
-  const phoneNum = document.getElementById("Addnum").value.trim();
-  const age = parseInt(document.getElementById("AddAge").value);
-  const email = document.getElementById("Addemail").value.trim();
-  const freeText = document.getElementById("AddFreeText").value.trim();
+  const name = document.getElementById('addName').value.trim();
+  const phoneNum = document.getElementById('Addnum').value.trim();
+  const age = parseInt(document.getElementById('AddAge').value);
+  const email = document.getElementById('Addemail').value.trim();
+  const freeText = document.getElementById('AddFreeText').value.trim();
 
   if (!name || !phoneNum) {
-    alert("Enter Valid Values");
+    alert('Enter Valid Values');
     return;
   }
   if (contactList.some(c => c.name.toLowerCase() === name.toLowerCase())) {
-    alert("this contact is already in the list");
+    alert('this contact is already in the list');
     return;
   }
   contactList.push({
@@ -158,63 +213,79 @@ document.querySelector('.addContactForm').onsubmit = e => {
     age,
     email,
     freeText,
-    dateAdded: new Date()
+    dateAdded: new Date(),
   });
   document.getElementById('addContact').style.display = 'none';
   sortContacts();
-  document.getElementById("addName").value = "";
-  document.getElementById("Addnum").value = "";
-  document.getElementById("AddAge").value = "";
-  document.getElementById("Addemail").value = "";
-  document.getElementById("AddFreeText").value = "";
+  document.getElementById('addName').value = '';
+  document.getElementById('Addnum').value = '';
+  document.getElementById('AddAge').value = '';
+  document.getElementById('Addemail').value = '';
+  document.getElementById('AddFreeText').value = '';
 };
 
-document.getElementById("addContactBtn").onclick = () => {
-  document.getElementById("addContact").style.display = 'block';
+document.getElementById('addContactBtn').onclick = () => {
+  document.getElementById('addContact').style.display = 'block';
 };
 
-document.getElementById("closeAddPopUpBtn").onclick = () => {
-  document.getElementById("addContact").style.display = 'none';
+document.getElementById('closeAddPopUpBtn').onclick = () => {
+  document.getElementById('addContact').style.display = 'none';
 };
 
-document.getElementById("closeInfoPopUpBtn").onclick = () => {
-  document.getElementById("contactInfo").style.display = 'none';
+document.getElementById('closeInfoPopUpBtn').onclick = () => {
+  document.getElementById('contactInfo').style.display = 'none';
 };
 
-document.getElementById("closebtnpopup2").onclick = () => {
-  document.getElementById("editContact").style.display = 'none';
+document.getElementById('closebtnpopup2').onclick = () => {
+  document.getElementById('editContact').style.display = 'none';
 };
 
 //Dark mode/light mode change
-const darkBtn = document.getElementById("darkBtn");
-darkBtn.addEventListener('click', (e) => {
+const darkBtn = document.getElementById('darkBtn');
+darkBtn.addEventListener('click', e => {
   e.preventDefault();
   if (document.querySelector('body').className == 'lightMode') {
-    document.getElementById('btnImageDark').setAttribute('src', './images/moon2.png');
+    document
+      .getElementById('btnImageDark')
+      .setAttribute('src', './images/moon2.png');
     document.querySelector('body').className = 'darkMode';
     document.querySelector('header').className = 'darkModeHeader';
-  }
-  else {
-    document.getElementById('btnImageDark').setAttribute('src', './images/moon1.png');
+  } else {
+    document
+      .getElementById('btnImageDark')
+      .setAttribute('src', './images/moon1.png');
     document.querySelector('body').className = 'lightMode';
     document.querySelector('header').className = 'lightModeHeader';
   }
-})
+});
 
 //search contact
 const searchBtn = document.getElementById('searchBtn');
 searchBtn.addEventListener('click', () => {
-  const nameToFind = document.getElementById('searchInput').value.trim().toLowerCase();
+  const nameToFind = document
+    .getElementById('searchInput')
+    .value.trim()
+    .toLowerCase();
   let found = false;
   for (let i = 0; i < contactList.length; i++) {
     if (contactList[i].name.toLowerCase() === nameToFind) {
       let contact = contactList[i];
       document.getElementById('name').textContent = contact.name;
-      document.getElementById('contactInfoP1').textContent = `Phone: ${contact.phoneNum}`;
-      document.getElementById('contactInfoP2').textContent = `Age: ${contact.age}`;
-      document.getElementById('contactInfoP3').textContent = `Email: ${contact.email}`;
-      document.getElementById('contactInfoP4').textContent = `Note: ${contact.freeText}`;
-      document.getElementById('contactInfoP5').textContent = `Added: ${contact.dateAdded.toLocaleString()}`;
+      document.getElementById(
+        'contactInfoP1',
+      ).textContent = `Phone: ${contact.phoneNum}`;
+      document.getElementById(
+        'contactInfoP2',
+      ).textContent = `Age: ${contact.age}`;
+      document.getElementById(
+        'contactInfoP3',
+      ).textContent = `Email: ${contact.email}`;
+      document.getElementById(
+        'contactInfoP4',
+      ).textContent = `Note: ${contact.freeText}`;
+      document.getElementById(
+        'contactInfoP5',
+      ).textContent = `Added: ${contact.dateAdded.toLocaleString()}`;
       document.getElementById('contactInfo').style.display = 'block';
       found = true;
       break;
@@ -223,14 +294,14 @@ searchBtn.addEventListener('click', () => {
   if (!found) {
     alert(`Sorry, ${nameToFind} is not in the phone book`);
   }
-  document.getElementById('searchInput').value = "";
+  document.getElementById('searchInput').value = '';
 });
 
 //delete all button
 const deleteAllBtn = document.getElementById('deleteAllBtn');
-deleteAllBtn.addEventListener('click', (e) => {
+deleteAllBtn.addEventListener('click', e => {
   e.preventDefault();
-  const confirmed = confirm("Are you sure you want to delete all contacts?");
+  const confirmed = confirm('Are you sure you want to delete all contacts?');
   if (confirmed) {
     contactList.length = 0;
     sortContacts();
